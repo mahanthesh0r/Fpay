27ce6abf9e949a7c76c956c68abd244155b26c61
Mahanthesh0r <monty.officio@gmail.com>
2021-03-01 00:22:34 +0530

    connected screens

--- app/src/main/java/com/mahanthesh/fpay/activity/LoginActivity.java
+++ app/src/main/java/com/mahanthesh/fpay/activity/LoginActivity.java
@@ -2,15 +2,50 @@ package com.mahanthesh.fpay.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.mahanthesh.fpay.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button btnSignup, btnSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE | WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+
+        init();
+        listener();
+    }
+
+    private void listener() {
+        btnSignup.setOnClickListener(this);
+        btnSignin.setOnClickListener(this);
+    }
+
+    private void init() {
+        btnSignin = findViewById(R.id.btn_signin);
+        btnSignup = findViewById(R.id.btn_create_account);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_signin:
+                Intent signinIntent = new Intent(this, HomeActivity.class);
+                startActivity(signinIntent);
+                break;
+            case R.id.btn_create_account:
+                Intent signupIntent = new Intent(this, SignupActivity.class);
+                startActivity(signupIntent);
+                break;
+        }
     }
 }
