f812cc5e987619933f51e8fff0bc5d784bb233f4
Mahanthesh0r <monty.officio@gmail.com>
2021-03-09 03:10:56 +0530

    added firebase auth ui

---
+++ app/src/main/java/com/mahanthesh/fpay/activity/ProfileActivity.java
@@ -0,0 +1,19 @@
+package com.mahanthesh.fpay.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.mahanthesh.fpay.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+
+    }
+}
